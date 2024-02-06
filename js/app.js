b0a0a03b290a748e6a380aeccddfed82ea913968
Ernesto <ernesto.shdz@outlook.com>
2024-02-05 19:37:39 -0600

    ya funcionan las clases como deben

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ import Basemaps from "./modulos/basemaps.js";
 import Layers from "./modulos/layers.js";
 import Peticiones from "./modulos/peticiones.js";
 import Estilos from "./modulos/estilos.js";
-//import Capas from "./modulos/capas.js";
+import Capas from "./modulos/capas.js";
 
 export default class App{
     constructor(){
+
+        //Inicializar propiedades
         this.basemaps = new Basemaps();
         this.layers = new Layers();
         this.peticiones = new Peticiones();
         this.estilos = new Estilos();
-        //this.capas = new Capas();
+        this.capas = new Capas();
     }
 
     load(){
@@ -19,30 +21,23 @@ export default class App{
         //Variable mapa
         const map = L.map('map').setView([38.548165, -98.833008], 4);
 
-        let capas = [
-            {
-                layer: this.layers.eua,
-                folder: 'USA',
-                nombre_archivo: 'us-states_es6'
-                //estilo: this.estilos.estilo_eua(),
-                //pop: euaPopup
-            },
-            {
-                layer: this.layers.mx_edos,
-                folder: 'MX',
-                nombre_archivo: 'México_Estados'
-                //estilo: this.estilos.estilo_mx(),
-                //pop: popup
-            }
-        ];
+        this.capas.ingresar({
+            layer: this.layers.eua,
+            folder: 'USA',
+            nombre_archivo: 'us-states_es6',
+            estilo: this.estilos.estilo_eua(),
+            pop: null
+        },);
+        this.capas.ingresar({
+            layer: this.layers.mx_edos,
+            folder: 'MX',
+            nombre_archivo: 'México_Estados',
+            estilo: this.estilos.estilo_mx(),
+            pop: null
+        });
 
-        /* this.capas.capas.forEach(item => {
-            console.log(typeof item.layer)
-            this.peticiones.getCapas(item.layer,item.folder,item.nombre_archivo,"","");
-        }); */
-
-        capas.forEach(item => {
-            this.peticiones.getCapas(item.layer,item.folder,item.nombre_archivo,"","");
+        this.capas.consultar().forEach(item => {
+            this.peticiones.getCapas(item.layer,item.folder,item.nombre_archivo,item.estilo,item.pop);
         });
 
         this.basemaps.osm.addTo(map);
